2bedf0e5f58b2c5eaa1b10f6ae22ae3c07a23cbe
Sean O'Daniels <sodaniels@gmail.com>
2023-11-27 15:50:22 -0500

    Solution for exercise 4.2 Blog list, step2. Refactored code so that everything lives in its own home.

---
+++ app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('Connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(result => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
