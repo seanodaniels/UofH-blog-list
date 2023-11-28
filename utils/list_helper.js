22a353d48d29605517358fa8a47d4ec589af1b3e
Sean O'Daniels <sodaniels@gmail.com>
2023-11-28 15:23:14 -0500

    Solution for exercise 4.6*: helper functions and unit tests, step4. Compile new array of objects describing authors blog totals, then pull the object out with the most blogs.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -30,8 +30,50 @@ const favoriteBlog = (blogs) => {
   return returnBlog
 }
 
+const mostBlogs = (blogs) => {
+  const totalsArray = []
+
+  for (blog in blogs) {
+    const currentAuthor = blogs[blog].author
+
+    if (totalsArray[0]) {
+
+      const blogFind = totalsArray.find((element) => currentAuthor === element.author)
+
+      if (blogFind) {
+        blogFind.blogs += 1
+      } else {
+        totalsArray.push({
+          author: blogs[blog].author, 
+          blogs: 1
+        })
+      }
+
+    } else {
+      totalsArray.push({
+        author: blogs[blog].author, 
+        blogs: 1
+      })
+    }
+  }
+
+  const reducer = (accum, item) => {
+    return item.blogs > accum.blogs
+      ? accum = item
+      : accum
+  }
+  
+  const topAuthor = totalsArray.reduce(reducer, { author:"", blogs:0 })
+
+  return {
+    author: topAuthor.author,
+    blogs: topAuthor.blogs
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
