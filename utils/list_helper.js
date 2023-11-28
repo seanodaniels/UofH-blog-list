f2f5a57ab6863932e4156dc0b90f510b881a9969
Sean O'Daniels <sodaniels@gmail.com>
2023-11-28 13:29:42 -0500

    Solution for exercise 4.4: helper functions and unit tests, step2. Test for total likes.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+  
+  const totalLikes = blogs.reduce(reducer, 0)
+
+  return totalLikes
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
