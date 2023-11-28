47b7e994021eff34be8d55c61e7ca4c6646251b5
Sean O'Daniels <sodaniels@gmail.com>
2023-11-28 14:11:13 -0500

    Solution for exercise 4.5*: helper functions and unit tests, step3. Used reducer to determine the blog with the greatest number of likes.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,7 +12,26 @@ const totalLikes = (blogs) => {
   return totalLikes
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (accum, item) => {
+    return item.likes > accum.likes
+      ? accum = item
+      : accum
+  }
+  
+  const topBlog = blogs.reduce(reducer, blogs[0])
+
+  const returnBlog = { 
+    title: topBlog.title, 
+    author: topBlog.author, 
+    likes: topBlog.likes 
+  }    
+
+  return returnBlog
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
