41cb3832a612b2c7e430a16175771b6dc39ff8e6
Sean O'Daniels <sodaniels@gmail.com>
2023-11-30 13:37:13 -0500

    Solution for exercise 4.11*: Blog list tests, step4. Tested for existence of the likes property and if it doesnt exist then set it to 0

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,6 +13,10 @@ blogsRouter.get('/', (request, response) => {
 blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
 
+  if (!blog.likes) {
+    blog.likes = 0
+  }
+
   blog
     .save()
     .then(result => {
