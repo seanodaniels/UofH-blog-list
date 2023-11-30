29a2de7a8b7c409b62fe796667414c2d6074294d
Sean O'Daniels <sodaniels@gmail.com>
2023-11-30 14:09:18 -0500

    Solution for exercise 4.11*: Blog list tests, step4. Modified routes to use async / await.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -21,7 +22,11 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,27 +2,25 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 // BEGIN ROUTES
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
 
-  if (!blog.likes) {
-    blog.likes = 0
-  }
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  })
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
+
 // END ROUTES
 
 module.exports = blogsRouter
