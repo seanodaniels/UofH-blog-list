61c882d85739fb315179182f2e03543e51ef1cd2
Sean O'Daniels <sodaniels@gmail.com>
2023-12-05 16:07:36 -0500

    Solution for exercise 4.16*: bloglist expansion, step4. Validation for username and password.

--- controllers/users.js
+++ controllers/users.js
@@ -2,21 +2,32 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  if (!password || password.length < 4) {
+    response.status(400).send('Password is required and must be at least 3 characters in length')
+    console.log('Password is required and must be at least 3 characters in length')
+  } else {
 
-  const user = new User({
-    username,
-    name,
-    passwordHash
-  })
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const savedUser = await user.save()
+    const user = new User({
+      username,
+      name,
+      passwordHash
+    })
 
-  response.status(201).json(savedUser)
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+  }
 })
 
 module.exports = usersRouter
