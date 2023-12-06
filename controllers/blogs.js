4bd2f44584e67beaa1aab296fadf989b6ced06c9
Sean O'Daniels <sodaniels@gmail.com>
2023-12-06 11:36:52 -0500

    Solution for exercise 4.17: bloglist expansion, step5.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 // BEGIN ROUTES
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user')
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const user = await User.findOne({})
 
   if (body.title && body.url) {
 
@@ -17,9 +20,13 @@ blogsRouter.post('/', async (request, response, next) => {
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
+      user: user.id
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
   } else {
     const errorMessage = !body.title
